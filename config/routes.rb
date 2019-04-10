f2da453182e55174de7568a2395b40fcc14f9f2c
Tyler Killgore <t.killgore.tk@gmail.com>
2019-04-10 11:11:32 -0400

    Second commit after installing bootstrap gem and navbar theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'pages/home'
-
 root 'pages#home'
+
+get 'about' => 'pages#about'
+
+get 'login' => 'pages#login'
+
 end
