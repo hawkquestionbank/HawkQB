77bb65ddab2340357ce14efc0fc4edf1cb110bb7
Tyler Killgore <t.killgore.tk@gmail.com>
2019-04-17 13:32:37 -0400

    installed simple_form gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # default url
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -60,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # default url
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
 end
