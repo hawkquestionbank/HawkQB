278086f14cf5136ed43bff8e4cb809eec3045b47
yangsong8 <ysong8@ncsu.edu>
2019-05-21 21:32:00 -0400

    Added register/login/logout links to the nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pages#home'
+  #
+  root 'pages#home'
 
-get 'about', to: 'pages#about'
+  get 'about', to: 'pages#about'
 
-get 'login' => 'pages#login'
+  get 'login' => 'pages#login'
 
-get 'users' => 'pages#users'
+  get 'users' => 'pages#users'
 
 end
