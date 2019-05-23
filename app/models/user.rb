65dab4e6e8afbd167ccdd0a47c15ab4da1c8e9ea
yangsong8 <ysong8@ncsu.edu>
2019-05-23 15:29:18 -0400

    Refactor full name method - remind user if it's too short

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable, :trackable
 
   def full_name
-    (first_name.nil? ? "":first_name) + ' ' + (last_name.nil? ? "":last_name)
+    combined_name = (first_name.nil? ? "":first_name) + ' ' + (last_name.nil? ? "":last_name)
+    if combined_name.length <= 1
+      combined_name = "please update your user name"
+    end
+    combined_name
   end
 end
