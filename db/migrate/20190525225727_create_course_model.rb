e58a249bca19ede8ad7c7980678f54ac85958896
yangsong8 <ysong8@ncsu.edu>
2019-05-26 23:49:51 -0400

    added course model.

---
+++ db/migrate/20190525225727_create_course_model.rb
@@ -0,0 +1,12 @@
+class CreateCourseModel < ActiveRecord::Migration[5.2]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+
+
+    end
+  end
+end
