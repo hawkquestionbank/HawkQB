ce9805a591c0c36abfef5c9a08ebe0ad0736eb82
yangsong8 <ysong8@ncsu.edu>
2019-05-29 15:50:30 -0400

    Fixed migration of CourseRegistration

--- db/migrate/20190529034130_create_course_registrations.rb
+++ db/migrate/20190529034130_create_course_registrations.rb
@@ -1,8 +1,8 @@
 class CreateCourseRegistrations < ActiveRecord::Migration[5.2]
   def change
     create_table :course_registrations do |t|
-      t.reference :user
-      t.reference :course
+      t.references :user
+      t.references :course
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_033901) do
+ActiveRecord::Schema.define(version: 2019_05_29_034130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "course_registrations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_course_registrations_on_course_id"
+    t.index ["user_id"], name: "index_course_registrations_on_user_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
