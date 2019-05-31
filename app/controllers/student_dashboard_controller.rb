1adfb4f6bc6374482872aed544dd3a2b8e5b6ecf
yangsong8 <ysong8@ncsu.edu>
2019-05-31 19:32:23 -0400

    Added student_dashboard/list

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -1,6 +1,7 @@
 class StudentDashboardController < ApplicationController
   access student: :all
   def list
-
+    # courses that the current user(student) has registered for
+    @courses = current_user.courses
   end
 end
