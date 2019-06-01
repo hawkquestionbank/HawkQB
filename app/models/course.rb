6b6643e4408e1fbcce1a57603c244f3ded347bc5
Tyler Killgore <t.killgore.tk@gmail.com>
2019-06-01 15:00:28 -0400

    added has many micro_credential thru maps table

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,12 @@
 class Course < ActiveRecord::Base
   belongs_to :creator_user, class_name: 'User', foreign_key: 'creator_user_id'
+  belongs_to :micro_credential_maps
 
   validates :title, presence: true
 
   has_many :course_resgistrations
   has_many :users, through: :course_registrations
+  has_many :micro_credential, through: :micro_credential_maps
+
+
 end
