3b30019289ba3782b32a5ad706a8d4fa695ba0b6
yangsong8 <ysong8@ncsu.edu>
2019-06-06 17:04:58 -0400

    Added identifier to MC DB table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_034951) do
+ActiveRecord::Schema.define(version: 2019_06_06_205959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_034951) do
   create_table "micro_credentials", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "creator_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,4 +65,5 @@ ActiveRecord::Schema.define(version: 2019_06_02_034951) do
   end
 
   add_foreign_key "courses", "users", column: "creator_user_id"
+  add_foreign_key "micro_credentials", "users", column: "creator_user_id"
 end
