22ec24776f01d55f0ee2c7aadffe08b5d3d5d5d8
yangsong8 <ysong8@ncsu.edu>
2019-06-15 20:30:24 -0400

    Added validations to MC model

--- app/models/micro_credential.rb
+++ app/models/micro_credential.rb
@@ -2,5 +2,7 @@ class MicroCredential < ActiveRecord::Base
 	has_many :micro_credential_maps
 	has_many :courses, through: :micro_credential_maps
 
+	validates_presence_of :title, :message => "can't be empty"
+	validates_presence_of :identifier, :message => "can't be empty"
   validates_length_of :identifier, :maximum => 20
 end
