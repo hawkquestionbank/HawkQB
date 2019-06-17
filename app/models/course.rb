16224878ee1b6270b7d3fc66950431baa08bc607
yangsong8 <ysong8@ncsu.edu>
2019-06-17 19:45:18 -0400

    Refactor creator_user -> creator

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  belongs_to :creator_user, class_name: 'User', foreign_key: 'creator_user_id'
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_user_id'
   has_many :micro_credential_maps
 
   validates_presence_of :title, :message => "can't be empty"
@@ -8,6 +8,5 @@ class Course < ActiveRecord::Base
   has_many :course_resgistrations
   has_many :users, through: :course_registrations
   has_many :micro_credentials, through: :micro_credential_maps
-
 
 end
