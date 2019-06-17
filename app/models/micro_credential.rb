a95635baa4f4e0f37abb3ac10e0956e1e0c46884
yangsong8 <ysong8@ncsu.edu>
2019-06-17 19:42:03 -0400

    Refactor the creator method with rails belongs_to

--- app/models/micro_credential.rb
+++ app/models/micro_credential.rb
@@ -2,11 +2,10 @@ class MicroCredential < ActiveRecord::Base
 	has_many :micro_credential_maps
 	has_many :courses, through: :micro_credential_maps
 
+	belongs_to :creator, class_name: 'User', foreign_key: 'creator_user_id'
+
 	validates_presence_of :title, :message => "can't be empty"
 	validates_presence_of :identifier, :message => "can't be empty"
   validates_length_of :identifier, :maximum => 20
 
-  def creator
-    User.find(self.creator_user_id)
-  end
 end
