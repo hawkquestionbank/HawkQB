afc5eebe57a8d13e375329bec86846c54a4b2ed9
yangsong8 <ysong8@ncsu.edu>
2019-06-28 16:41:04 -0400

    Implemented sti paths for potential actions on different question types

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,14 @@
 module QuestionsHelper
+  # Returns a dynamic path based on the provided parameters
+  def sti_question_path(type = "question", question = nil, action = nil)
+    send "#{format_sti(action, type, question)}_path", question
+  end
+
+  def format_sti(action, type, question)
+    action || question ? "#{format_action(action)}#{type.underscore}" : "#{type.underscore.pluralize}"
+  end
+
+  def format_action(action)
+    action ? "#{action}_" : ""
+  end
 end
