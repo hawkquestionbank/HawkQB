45ac652bfe6583239ea7b560c0237a309fac0db6
yangsong8 <ysong8@ncsu.edu>
2019-07-11 04:42:07 -0400

    Find the unused micro-credentials created by the current user

--- app/models/micro_credential.rb
+++ app/models/micro_credential.rb
@@ -8,4 +8,18 @@ class MicroCredential < ActiveRecord::Base
 	validates_presence_of :identifier, :message => "can't be empty"
   validates_length_of :identifier, :maximum => 20
 
+  def self.available_micro_credentials user
+		if user.role == :student
+			[]
+		else
+			self_created_micro_credentials = user.own_micro_credentials
+			self_created_micro_credential_ids = self_created_micro_credentials.map(&:id)
+
+			used_micro_credential_ids = MicroCredentialMap.distinct.pluck(:micro_credential_id)
+
+			# find the ids in self_created_micro_credential_ids but not in used_micro_credential_ids
+			MicroCredential.find(self_created_micro_credential_ids - used_micro_credential_ids)
+		end
+	end
+
 end
