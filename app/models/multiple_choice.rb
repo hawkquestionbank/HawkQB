91363f6b771c6ed24e50af4973cc2b7a5730c398
yangsong8 <ysong8@ncsu.edu>
2019-07-23 11:36:46 -0400

    Added valid_attempt to MultipleChoice

--- app/models/multiple_choice.rb
+++ app/models/multiple_choice.rb
@@ -1,3 +1,10 @@
 class MultipleChoice < Question
   scope :multiple_choices, -> { where(type: 'MultipleChoice') }
+
+  def valid_attempt? attempt
+    # validate an attempt prepared by attempt_controller/create
+    # MultipleChoice: there is always a selected answer from UI
+    # So simply return true
+    true
+  end
 end
