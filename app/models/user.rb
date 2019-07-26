953b1ae9a2b5eacba8d2bf7cd70c922969d684fe
yangsong8 <ysong8@ncsu.edu>
2019-07-26 06:50:42 -0400

    Show closed and open question numbers on student_dashboard/list.

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,13 +38,30 @@ class User < ActiveRecord::Base
     self.id == target_object.creator_user_id
   end
 
-  def finished_question question, max_attempts, close_to_attempts
-    # find the attempts of the current user on the question
-    # return true if one of the conditions is true
-    # 1) when max_attempts is a integer: the number of **finished** attempts equals to or higher than max_attempts
-    # 2) when max_attempts is nil
-    # we do not check the close_to_attempts time, if a question is past due but a student has not tried it, it is still
-    # unfinished.
+  def open_closed_questions_counts
+    # find the #s of attempts of the current user on the question in all the registered courses
+    # for each questions,
+    # 1) when max_attempts is a integer: the number of attempts equals to or higher than max_attempts  => closed
+    # 2) when closed_to_attempts is not nil and this time is already passed => closed
+    # 3) otherwise => open
+    open_closed_questions_counter = {}
+    self.courses.each do |course|
+      open_closed_questions_counter[course.id] = {}
+      open_closed_questions_counter[course.id][:open] = 0
+      open_closed_questions_counter[course.id][:closed] = 0
+      attempts = Attempt.where(:user_id=>self.id).joins(:question).where("questions.course_id=?", course.id ).order("id desc")
+      course.questions.each do |question|
+        if not course.close_to_attempts.nil? and DateTime.now > course.close_to_attempts
+          open_closed_questions_counter[course.id][:closed] += 1
+        elsif not course.max_attempts.nil?
+          attempts.where("question_id=?", question.id).size >= course.max_attempts
+          open_closed_questions_counter[course.id][:closed] += 1
+        else
+          open_closed_questions_counter[course.id][:open] += 1
+        end
+      end
+    end
+    open_closed_questions_counter
   end
 
   def can_see_correct_answer question, can_view_answers_after
