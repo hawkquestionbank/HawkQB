0beecf2761ef963cc285fec926e1bdfc42392fa1
yangsong8 <ysong8@ncsu.edu>
2019-07-28 04:22:15 -0400

    Added can_see_correct_answers? to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -20,4 +20,18 @@ class Course < ActiveRecord::Base
       DateTime.now > self.close_to_attempts
     end
   end
+
+  def can_see_correct_answers?
+    # Determine whether to display
+    # if this course is an **exam** (only), compare current time and can_view_answers_after and close_to_attempts, which ever is later
+    # if this course is **not** an exam, can_view_answers_after is not empty, compare current time and can_view_answers_after
+    # otherwise, by default, students **cannot** see correct answers
+    if self.is_an_exam
+      DateTime.now > self.can_view_answers_after and DateTime.now > self.can_view_answers_after
+    elsif not self.can_view_answers_after.nil?
+      DateTime.now > self.can_view_answers_after
+    else
+      false
+    end
+  end
 end
