9660a851112f88900880994e7f384fc286e44513
yangsong8 <ysong8@ncsu.edu>
2019-08-04 17:50:23 -0400

    Implemented creating and grading attempts for MultipleSelect.

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -26,8 +26,20 @@ class AttemptsController < ApplicationController
 
   def create
     @attempt = Attempt.new(attempt_params)
-    @attempt.answer_id = params[:quiz_question_choices][:selected_index]
     @question = Question.find(params[:attempt][:question_id])
+    if @question.type == "MultipleChoice"
+      @attempt.answer_id = params[:quiz_question_choices][:selected_index]
+    elsif @question.type == "MultipleSelect"
+      #{"utf8"=>"✓", "authenticity_token"=>"UNuBAnyln83j5CU+J9SFko7xFgFwIar50kW5drSaeeItpT0ImMzIkq0Jyq1aoxASWO4feeqyOHyF31p9scpACQ==",
+      # "quiz_question_choices"=>{"91"=>{"selected_index"=>"91"}, "94"=>{"selected_index"=>"94"}},
+      # "course_id"=>"7", "attempt"=>{"user_id"=>"4", "question_id"=>"47", "created_at"=>"2019-08-04 01:47:35 -0400"},
+      # "commit"=>"Create Attempt"}
+      if params.key?("quiz_question_choices")
+        @attempt.answer_hash = {"answer_ids"=>params["quiz_question_choices"].keys}
+      else
+        @attempt.answer_hash = {"answer_ids"=>[]}
+      end
+    end
 
     if not @question.valid_attempt?(@attempt)
       redirect_to ({controller: "attempts", action: "new", course_id: params[:course_id], question_id: @question.id}), alert:'ERROR: invalid answer, please make sure you have answered the question.'
--- app/models/multiple_select.rb
+++ app/models/multiple_select.rb
@@ -1,3 +1,35 @@
 class MultipleSelect < Question
   scope :multiple_selects, -> { where(type: 'MultipleSelect') }
+
+  def valid_attempt? attempt
+    # for MultipleSelect attempt, we have to use @attempt.answer_hash.
+    # It is guaranteed that the key "answer_ids" is in answer_hash,
+    # we need to check if there are any answer id(s) there.
+    attempt.answer_hash["answer_ids"].length != 0
+  end
+
+  def correct_answers
+    # return all the correct answers of this question
+    self.answers.where(:is_correct => 1)
+  end
+
+  def grade attempt
+    # assign 1 (correct) or (0) incorrect to attempt.score
+    # sanple attempt object:
+    # <Attempt id: nil, txt: nil, question_id: 46, user_id: 3, answer_id: 87, created_at: "2019-07-26 12:39:03", updated_at: nil, answer_hash: {}, score: nil>
+    # (please not that, the attempt object is not in DB yet)
+    correct_answer_ids = self.correct_answers.pluck(:id)
+    user_selected_answer_ids = attempt.answer_hash["answer_ids"]
+
+    if correct_answer_ids.size ==0 or user_selected_answer_ids.size ==0
+      false
+
+    elsif correct_answer_ids.size == user_selected_answer_ids.size and (correct_answer_ids.map(&:to_s) & user_selected_answer_ids).size == correct_answer_ids.size
+      attempt.score = 1
+      true
+    else
+      attempt.score = 0
+      true
+    end
+  end
 end
