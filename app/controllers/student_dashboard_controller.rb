d83490be2e175a5b5ccaf6bca0e501eee2f5343e
ztcampbell <ztc1995@gmail.com>
2020-02-05 10:54:47 -0500

    Added a function to allow us to call the course percentage into the gauge graph.

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -6,6 +6,22 @@ class StudentDashboardController < ApplicationController
   end
 
   def individual_progress
+    @course = Course.find(params[:course_id])
+    total_score_in_course = 0
+    questions = @course.questions
+    questions.each do |question|
+      attempts = Attempt.where(question_id: question.id, user_id: current_user.id).order({ id: :asc })
+      if attempts.any?  # there is at least one attempt
+        latest = attempts.last
+        total_score_in_course += latest.score
+      end
+    end
+
+    if questions.any?
+      @total_percentage = 100 * total_score_in_course/questions.length/questions.length
+    else
+      @total_percentage = 0
+    end
 
   end
 end
