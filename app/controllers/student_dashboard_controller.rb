0809c0157d0702dedfba770673885a5d075b78f6
ztcampbell <ztc1995@gmail.com>
2020-02-19 10:52:35 -0500

    Added the mock up Aster Plot

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -7,8 +7,16 @@ class StudentDashboardController < ApplicationController
 
   def individual_progress
     @course = Course.find(params[:course_id])
+
+    @total_percentage = individual_percentage(@course)
+
+    @competency_aster_csv = 'id,score,weight,color,label\nFIS,50,0.5,#9E0041,Fisheries\nMAR,100,0.5,#C32F4B,Mariculture\nNP,100,1,#F47245,Natural Products\nCS,100,1,#FB9F59,Carbon Storage\n'
+  end
+
+  private
+  def individual_percentage(course)
     total_score_in_course = 0
-    questions = @course.questions
+    questions = course.questions
     questions.each do |question|
       attempts = Attempt.where(question_id: question.id, user_id: current_user.id).order({ id: :asc })
       if attempts.any?  # there is at least one attempt
@@ -18,10 +26,10 @@ class StudentDashboardController < ApplicationController
     end
 
     if questions.any?
-      @total_percentage = 100 * total_score_in_course/questions.length/questions.length
+      total_percentage = 100 * total_score_in_course/questions.length/questions.length
     else
-      @total_percentage = 0
+      total_percentage = 0
     end
-
+    total_percentage
   end
 end
